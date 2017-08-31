7e5972156d992fa850d799f1b9993ef4607bac05
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-31 13:57:19 +0800

    all

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,7 +7,7 @@ class CitiesController < ApplicationController
     city = City.find(params[:id])
 
     response = RestClient.get "http://v.juhe.cn/weather/index",
-                              :params => { :cityname => city.juhe_id, :key => "87938b98761fb4a872987bee12bc3abd" }
+                              :params => { :cityname => city.juhe_id, :key => JUHE_CONFIG["api_key"] }
     data = JSON.parse(response.body)
 
     city.update( :current_temp => data["result"]["sk"]["temp"] )
