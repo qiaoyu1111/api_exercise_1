cb61ec41ce65feca72af6f625ad2fd1e372ec035
qiaoyu <qiaoyu1111358@gmail.com>
2017-08-31 11:16:15 +0800

    add add

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,4 +2,16 @@ class CitiesController < ApplicationController
   def index
     @cities = City.all
   end
+
+  def update_temp
+    city = City.find(params[:id])
+
+    response = RestClient.get "http://v.juhe.cn/weather/index",
+                              :params => { :cityname => city.juhe_id, :key => "87938b98761fb4a872987bee12bc3abd" }
+    data = JSON.parse(response.body)
+
+    city.update( :current_temp => data["result"]["sk"]["temp"] )
+
+    redirect_to cities_path
+  end
 end
